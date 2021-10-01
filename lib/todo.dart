53136591c3675bf7f3cffb95a00a25bd8f2d7694
Stas <holidayonmail@gmail.com>
2021-10-01 17:52:36 +0300

    added addTo toggle edit in TodoList StateNotifier

---
+++ lib/todo.dart
@@ -0,0 +1,74 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:uuid/uuid.dart';
+
+Uuid _uuid = Uuid();
+
+class Todo {
+  Todo({
+    required this.description,
+    String? id,
+    this.isCompleted = false,
+  }) : id = id ?? _uuid.v4();
+
+  final String description;
+  final String id;
+  final bool isCompleted;
+
+  @override
+  String toString() =>
+      'Todo(description: $description, id: $id, isCompleted: $isCompleted)';
+
+  Todo copyWith({
+    String? description,
+    String? id,
+    bool? isCompleted,
+  }) {
+    return Todo(
+      description: description ?? this.description,
+      id: id ?? this.id,
+      isCompleted: isCompleted ?? this.isCompleted,
+    );
+  }
+}
+
+class TodoList extends StateNotifier<List<Todo>> {
+  TodoList(List<Todo> initialTodos) : super(initialTodos);
+
+  void addTodo(String description) {
+    state = [
+      ...state,
+      Todo(
+        description: description,
+      ),
+    ];
+  }
+
+  void toggle(String id) {
+    state = [
+      for (final todo in state)
+        if (todo.id == id)
+          // Todo(
+          //   id: todo.id,
+          //   isCompleted: !todo.isCompleted,
+          //   description: todo.description,
+          // )
+          todo.copyWith(
+            isCompleted: !todo.isCompleted,
+          )
+        else
+          todo,
+    ];
+  }
+
+  void edit({required String id, required String description}) {
+    state = [
+      for (final todo in state)
+        if (todo.id == id)
+          todo.copyWith(
+            description: description,
+          )
+        else
+          todo,
+    ];
+  }
+}
